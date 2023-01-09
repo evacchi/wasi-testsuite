912c17677f14f784daab4a6616d145ed323737a3
Achille Roussel <achille.roussel@gmail.com>
2023-01-09 18:32:55 +0000

    fix wazero adapter
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- adapters/wazero.sh
+++ adapters/wazero.sh
@@ -2,12 +2,19 @@
 
 TEST_FILE=
 ARGS=()
-PROG_ARGS=()
+PROG_ARGS=("--")
 
 while [[ $# -gt 0 ]]; do
     case $1 in
     --version)
-        exec wazero version
+        # The test runner expects a version printed out in two parts, the first
+        # is the name of the runtime and the second is the version number.
+        version=$(wazero version)
+        if [ "$version" == "dev" ]; then
+            version=0.0.0
+        fi
+        echo wazero $version
+        exit 0
         ;;
     --test-file)
         TEST_FILE="$2"
@@ -36,8 +43,16 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+# Do not inject the "--" separator if there are no arguments, or wazero errors.
+if [ "${#PROG_ARGS[@]}" -eq 1 ]; then
+    PROG_ARGS=()
+fi
+
+# Run the tests in the test directory because the --dir arguments are given as
+# paths relative to the test file.
 TEST_DIR=$(dirname "$TEST_FILE")
 TEST_FILE=$(basename "$TEST_FILE")
 
 cd "$TEST_DIR"
-exec wazero run -hostlogging filesystem "${ARGS[@]}" "$TEST_FILE" -- "${PROG_ARGS[@]}"
+set -x # echo the next line, useful for debugging to re-run failing tests
+exec wazero run -hostlogging filesystem "${ARGS[@]}" "$TEST_FILE" "${PROG_ARGS[@]}"
